b673fa8c1a02b9c706cd42321672ffca4cc44103
lzy <2054758436@qq.com>
2025-09-07 17:41:06 +0800

    second commit

--- untitled/src/main/java/com/hamster/git/test.java
+++ untitled/src/main/java/com/hamster/git/test.java
@@ -3,5 +3,6 @@ package com.hamster.git;
 public class test {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello World2");
     }
 }
